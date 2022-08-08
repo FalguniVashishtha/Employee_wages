6f040e2e4b92934bea202097a981dae452f85f3c
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-08 18:12:46 +0530

    manage wage for multiple company

--- src/com/employeewages/employeewages/EmployeeWages.java
+++ src/com/employeewages/employeewages/EmployeeWages.java
@@ -19,14 +19,13 @@ public class EmployeeWages {
     //Computation of company wage
     public void companyWage(){
         System.out.printf("%s Employee Wage details: ", company).println();
-
+        //local variables
         int empWage = 0, totalWage=0, workingHours = 0, empHours = 0;
         Random random = new Random();
-
         //Calculating the total wage per month
         for (int day = 1; day<=workingDaysPerMonth && workingHours<maxWorkingHours; day++) {
             //Max Working hours should be 100.
-            int empCheck = random.nextInt(3);
+            int empCheck = random.nextInt();
             switch (empCheck) {
                 case 1:
                     System.out.printf("Day %d: Employee is Present for full day.", day).println();
@@ -42,26 +41,28 @@ public class EmployeeWages {
                     break;
             }
             empWage = empHours * EMP_RATE_PER_HOUR;
-            System.out.printf("Worked: " +empHours+ "hours, Wage:$" +empWage).println("\n");
+            System.out.printf("Worked:"+empHours+"hours, Wage:$" +empWage).println("\n");
             totalWage += empWage;
             workingHours += empHours; //Max Working Hours is 100, loop terminates if it is above 100.
         }
         totalEmpWage = workingHours*EMP_RATE_PER_HOUR;
-        System.out.printf("The Total wage of "+company+ "for this month: $ " + totalWage).println();
-        System.out.printf("Total hours of working for "+company+": " + workingHours ).println();
+        System.out.printf("The Total wage of " +company+ "for this month: $ " + totalWage).println();
+        System.out.printf("Total hours of working for " +company+ ": " + workingHours ).println();
         System.out.println();
     }
     //overriding the toString() method
     public String toString(){
         return "Total wage  for the Company " + company + " is: " + totalEmpWage;
     }
-    //main method.
-    public static void main(String args[]) {
+    // main method.
+    public static void main(String args[])
+    {
         //Welcome message
-        System.out.println("Welcome to Employee Wage page. \n");
-        EmployeeWages company1 = new EmployeeWages("HCL", 20, 20, 100);
+        System.out.println("Welcome to Employee Wage \n");
+
+        EmployeeWages company1 = new EmployeeWages("HCL", 20, 30, 150);
         company1.companyWage();
-        EmployeeWages company2 = new EmployeeWages("TCS", 20, 20, 200);
+        EmployeeWages company2 = new EmployeeWages("TCS", 15, 15, 200);
         company2.companyWage();
         System.out.println(company1);
         System.out.println(company2);
