71016918990c7660cd23c776c1b63d4af1009702
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-08 16:47:09 +0530

    calculate employee wages

--- src/com/employeewages/employeewages/EmployeeWages.java
+++ src/com/employeewages/employeewages/EmployeeWages.java
@@ -1,17 +1,27 @@
 package com.employeewages.employeewages;
 import java.util.Random;
 public class EmployeeWages {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employeewage page");
-        int IS_fULL_tIME = 1;
-        Random random = new Random();
-        int empCheck = random.nextInt();
-        if (empCheck == IS_fULL_tIME) {
-            System.out.println("Employee is present");
-        } else {
-            System.out.println("Employee is absent");
+    public static void main(String[] args)
+    {
+            System.out.println("Welcome to Employee Wage page");
+            int IS_fULL_tIME = 1;
+            int EMP_RATE_PER_HOUR=20;
+            int empHours=0;
+            int empWage=0;
+            Random random = new Random();
+            int empCheck = random.nextInt();
 
-        }
+            if (empCheck == IS_fULL_tIME)
+            {
+                System.out.println("Employee is present");
+                empHours=8;
+            } else
+            {
+                System.out.println("Employee is absent");
+                empWage = empHours*EMP_RATE_PER_HOUR;
+                System.out.println("The daliy wage of Empolyee is " + empWage);
+            }
+
     }
 
 }
