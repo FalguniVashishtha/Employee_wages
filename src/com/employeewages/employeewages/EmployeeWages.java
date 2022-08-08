75e5bc54f087ef135e5a69cefa20de7419177798
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-08 17:02:07 +0530

    switch case

--- src/com/employeewages/employeewages/EmployeeWages.java
+++ src/com/employeewages/employeewages/EmployeeWages.java
@@ -1,31 +1,33 @@
 package com.employeewages.employeewages;
 import java.util.Random;
 public class EmployeeWages {
-    public static void main(String args[]) {
+    final   static  int FULL_TIME_WORKER=1;
+    final static  int PART_TIME_WORKER=2;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage page");
+        int EMP_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
 
-        System.out.println("Welcome to Employeewage page");
-
-        int IS_FUll_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
-        int empHours = 0;
-        int empWage = 0;
         Random random = new Random();
-        int empCheck = random.nextInt();
+        int empCheck = random.nextInt(2);
 
-        if(empCheck == IS_FUll_TIME) {
-            System.out.println("Employee is Present.");
-            empHours = 8;
+
+        switch(empCheck){
+            case FULL_TIME_WORKER:
+                empHours=8;
+                System.out.println("Employee is present");
+                break;
+            case PART_TIME_WORKER:
+                empHours=4;
+                System.out.println("Employee is present");
+                break;
+            default:
+
+                System.out.println("Employee is absent");
         }
-        else if(empCheck == 2)
-        {
-            System.out.println("Employee worked for half day.");
-            empHours = 4;
-        }
-        else {
-            System.out.println("Employee is Absent.");
-        }
+
         empWage = empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage is " + empWage);
+        System.out.println("The daily wage of Employee is " + empWage);
     }
-
 }
