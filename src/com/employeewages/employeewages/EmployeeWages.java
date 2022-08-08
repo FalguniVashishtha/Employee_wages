4e82d5bb7256b9f10fb7d9c9843a052f3f1d4982
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-08 17:57:59 +0530

    save the total wage

--- src/com/employeewages/employeewages/EmployeeWages.java
+++ src/com/employeewages/employeewages/EmployeeWages.java
@@ -10,15 +10,15 @@ public class EmployeeWages {
 
 
     //Constructor for the class
-    public EmployeeWages(String company, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours)
-    {
+    public EmployeeWages(String company, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours){
         this.company = company;
         this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
         this.workingDaysPerMonth = workingDaysPerMonth;
         this.maxWorkingHours = maxWorkingHours;
     }
+    //Computation of company wage
     public void companyWage(){
-        System.out.printf("Employee Wage details: ", company).println();
+        System.out.printf("%s Employee Wage details: ", company).println();
 
         int empWage = 0, totalWage=0, workingHours = 0, empHours = 0;
         Random random = new Random();
@@ -27,8 +27,7 @@ public class EmployeeWages {
         for (int day = 1; day<=workingDaysPerMonth && workingHours<maxWorkingHours; day++) {
             //Max Working hours should be 100.
             int empCheck = random.nextInt(3);
-            switch (empCheck)
-            {
+            switch (empCheck) {
                 case 1:
                     System.out.printf("Day %d: Employee is Present for full day.", day).println();
                     empHours = 8;
@@ -43,7 +42,7 @@ public class EmployeeWages {
                     break;
             }
             empWage = empHours * EMP_RATE_PER_HOUR;
-            System.out.printf("Worked: "+ empHours + "hours, Wage:$" +empWage).println("\n");
+            System.out.printf("Worked: " +empHours+ "hours, Wage:$" +empWage).println("\n");
             totalWage += empWage;
             workingHours += empHours; //Max Working Hours is 100, loop terminates if it is above 100.
         }
@@ -59,10 +58,10 @@ public class EmployeeWages {
     //main method.
     public static void main(String args[]) {
         //Welcome message
-        System.out.println("Welcome to Employee Wage Builder. \n");
-        EmployeeWages company1 = new EmployeeWages("Bridgelabz", 20, 20, 100);
+        System.out.println("Welcome to Employee Wage page. \n");
+        EmployeeWages company1 = new EmployeeWages("HCL", 20, 20, 100);
         company1.companyWage();
-        EmployeeWages company2 = new EmployeeWages("TCS", 15, 30, 200);
+        EmployeeWages company2 = new EmployeeWages("TCS", 20, 20, 200);
         company2.companyWage();
         System.out.println(company1);
         System.out.println(company2);
