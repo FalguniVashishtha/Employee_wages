ef54a54d3317f2be624e536598f78a4ec00246e1
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-09 18:24:04 +0530

    daily wage & total wage

--- src/com/employeewages/employeewages/CompanyEmpWage.java
+++ src/com/employeewages/employeewages/CompanyEmpWage.java
@@ -6,7 +6,6 @@ public class CompanyEmpWage {
     final int WAGE_PER_HR;
     final int MAX_WORKING_DAYS;
     final int MAX_WORKING_HRS;
-    // instance variable
     int totalEmpWage;
     //Constructor
     CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
@@ -16,13 +15,12 @@ public class CompanyEmpWage {
         MAX_WORKING_HRS = maxWorkingHrs;
         totalEmpWage = 0;
     }
-    void setTotalEmployeeWage(int totalEmpWage) {
+    void setTotalEmployeeWage(int totalEmpWage)
+    {
         this.totalEmpWage = totalEmpWage;
     }
     public String toString() {
-        System.out.println("************************************************************");
         System.out.println("Details of " + COMPANY_NAME + " employee");
-        System.out.println("************************************************************");
         System.out.println("Wage per hour:" + WAGE_PER_HR);
         System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
         System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
--- src/com/employeewages/employeewages/EmpWageBuilder.java
+++ src/com/employeewages/employeewages/EmpWageBuilder.java
@@ -2,32 +2,35 @@ package com.employeewages.employeewages;
 
 import java.util.Random;
 import java.util.ArrayList;
-interface IEmpWageBuilder {
+import java.util.HashMap;
+//implementing interface
+interface IEmpWageBuilder
+{
     public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
     public void companyWage();
 }
 
-class EmpWageBuilder implements IEmpWageBuilder {
-    // instance variables
+class EmpWageBuilder implements IEmpWageBuilder
+{
     int noOfCompanies, index;
     ArrayList<CompanyEmpWage> companies; //ArrayList declaration
-
-    //Constructor for the class
+    HashMap<String, Integer> fullMonthWage; //HashMap Declaration, String = Company name, Integer = Full month wage.
     public EmpWageBuilder()
     {
-        companies = new ArrayList<>();
+        companies = new ArrayList<>(); //Creating Array list
+        fullMonthWage = new HashMap<>(); //Creating Hash Map
     }
-
-    //Assigning to the array
-    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+    //Assigning array
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
         CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
         companies.add(company);
+        fullMonthWage.put(companyName,0);
     }
     int companyWage(CompanyEmpWage companyEmpWage)
     {
         System.out.println("Computation of total wage of " + companyEmpWage.COMPANY_NAME + " employee:");
         System.out.printf("%1s   %5s   %5s   %5s\n", "Day", "Work_hrs", "Total_Wage", "Total_working_hrs");
-
 
         int workingHrs, totalWage = 0;
         for (int day = 1, totalWorkingHrs = 0; day <= companyEmpWage.MAX_WORKING_DAYS
@@ -37,16 +40,15 @@ class EmpWageBuilder implements IEmpWageBuilder {
             workingHrs = getWorkingHrs(empType); //Full time, Part time or Absent
             int wage = workingHrs * companyEmpWage.WAGE_PER_HR;
             totalWage += wage;
-            System.out.printf("%2d %5d %9d %12d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+            System.out.printf("%2d   %5d   %9d   %12d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
+        fullMonthWage.put(companyEmpWage.COMPANY_NAME, totalWage); // insert an entry in the map. key, value)
         return totalWage;
     }
-
     int generateEmployeeType() {
         Random random = new Random();
         return random.nextInt();
     }
-
     int getWorkingHrs(int empType) {
         switch (empType) {
             case 1:
@@ -57,7 +59,6 @@ class EmpWageBuilder implements IEmpWageBuilder {
                 return 0;
         }
     }
-
     public void companyWage() {
         for (CompanyEmpWage company : companies) //for-each loop
         {
@@ -66,14 +67,21 @@ class EmpWageBuilder implements IEmpWageBuilder {
             System.out.println(company); //overriding the toString() method
         }
     }
+    void printTotalEmpWages(){
+        System.out.println("The Companies and the full Month wage details of the Employee:");
+        for (String companyName : fullMonthWage.keySet()) //It is used to return a set view of the keys contained in this map.
+        {
+            System.out.println(companyName + ": " + fullMonthWage.get(companyName)); //Get method to get the value of key value.
+        }
+    }
 
-
-    public static void main(String args[]) {
-
+    public static void main(String args[])
+    {
         System.out.println("Welcome to Employee Wage Builder. \n");
         EmpWageBuilder emp = new EmpWageBuilder();
         emp.addCompany("Bridgeabz", 20, 20, 100);
-        emp.addCompany("TCS", 30, 20, 120);
-        emp.companyWage();
+        emp.addCompany("TCS", 25, 25, 2200);
+        emp.companyWage(); // Daily Wage along with the Total Wage
+        emp.printTotalEmpWages(); // Total Wage of the month of all companies.
     }
 }
